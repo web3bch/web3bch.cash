b3d39b8a09c9643a60af6fccff2a28e1fc9a2fb9
Kite0301 <kaito.titech@gmail.com>
2018-10-31 18:50:02 +0900

    Add animation to jumbotron content

--- js/main.js
+++ js/main.js
@@ -1,4 +1,37 @@
 $(function(){
+  $(window).on('load', () => {
+    const $jumbotronText = $("#jumbotron-text")
+    const children = $jumbotronText.children('div')
+    const texts = []
+    children.each((i, e) => {
+      texts.push($(e).text())
+      $(e).text("")
+    })
+    $jumbotronText.css({opacity: 1})
+    let line = 0;
+    let index = 0;
+    const insertText = () => {
+      if (line > 0 && index === 0) {
+        $(children[line - 1]).css({animation: "none", borderColor: "transparent"})
+      }
+      let timeout = 70
+      const $el = $(children[line])
+      const text = texts[line]
+      $el.text($el.text() + text[index])
+      index++
+      if (text.length === index) {
+        line++
+        index = 0
+        timeout += 700
+      }
+      if (line === texts.length) {
+        return
+      }
+      setTimeout(insertText, timeout)
+    }
+    setTimeout(insertText, 500)
+  })
+
   $(window).scroll(() => {
     var scrollRate = $(this).scrollTop() / ($("body").height() - window.innerHeight)
     var btnTop = (window.innerHeight - 20) * Math.min(scrollRate, 1)
